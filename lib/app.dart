12b04ee808eb053c67283f17e02ae63b8cdf16f8
Nguyen Thanh Sang <sangntse180064@fpt.edu.vn>
2026-06-15 17:22:48 +0700

    Add UI explore

--- lib/app.dart
+++ lib/app.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
+import 'screens/explore/explore_screen.dart';
 
 class FlexFitApp extends StatelessWidget {
   const FlexFitApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'FlexFit',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('FlexFit'),
-        ),
-        body: const Center(
-          child: Text('FlexFit Mobile'),
-        ),
-      ),
+      home: ExploreScreen(),
     );
   }
 }
